5d534e87177e4550900d1823e3f8f1e973c8c4c2
siobhan leese <siobhan.leese@hotmail.co.uk>
2024-07-08 20:30:56 +0100

    globawob

--- plugin/source/PluginEditor.cpp
+++ plugin/source/PluginEditor.cpp
@@ -18,13 +18,54 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 //==============================================================================
 void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 {
+    juce::Colour palePinkPeach = juce::Colour::fromRGB(250, 220, 231); //waveycolours
+    juce::Colour duskyLilac = juce::Colour::fromRGB(196, 169, 220);
 
-    g.fillAll (juce::Colours::paleturquoise);
+    juce::Colour backgroundColour = juce::Colour::fromRGB(255, 201, 213); // Background colour for the text
+    juce::Colour textColour = juce::Colour::fromRGB(164, 143, 183); // Colour for the text
 
-    g.setColour (juce::Colours::magenta);
-    g.fillRect (20, 20, 100, 100);
-    g.setFont (15.0f);
-    g.drawFittedText ("Soy Estupida", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll (duskyLilac);
+
+    g.setColour (palePinkPeach);
+    juce::Rectangle<float> backgroundRect = getLocalBounds().toFloat();
+    int numWaves = 60;
+    float waveAmplitude = 20.0f;
+    float waveFrequency = 0.05f;
+    float lineThickness = 10.0f;
+
+     for (int i = 0; i < numWaves; ++i)
+    {
+        juce::Path wavePath;
+        wavePath.startNewSubPath (backgroundRect.getX(), backgroundRect.getY() + i * waveAmplitude);
+
+        for (float x = backgroundRect.getX(); x < backgroundRect.getRight(); x += 1.0f)
+        {
+            float y = backgroundRect.getY() + i * waveAmplitude + std::sin(x * waveFrequency) * waveAmplitude;
+            wavePath.lineTo (x, y);
+        }
+
+        g.strokePath (wavePath, juce::PathStrokeType (lineThickness));
+    }
+
+     // Determine the text area
+    juce::Rectangle<int> textArea = getLocalBounds();
+    textArea = textArea.withSizeKeepingCentre(textArea.getWidth() / 2, textArea.getHeight() / 4);
+    textArea.setY(10); // Align to the top
+
+    // Fill the background for the text
+    g.setColour(backgroundColour);
+    g.fillRect(textArea);
+
+    // Draw the text
+    g.setColour(textColour);
+    juce::Font font;
+    font.setHeight(24.0f); // Set the desired font size
+    font.setBold(true);
+    g.setFont(font);
+    textArea = textArea.withSizeKeepingCentre(textArea.getWidth() / 2, textArea.getHeight() / 4);
+    textArea.setY(20); // Align to the top
+
+    g.drawFittedText("esselleAudio", textArea, juce::Justification::centred, 1);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
